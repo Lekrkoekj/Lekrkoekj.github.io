74c1ee24c44dcf561d080874e991f4bb07813171
Lekrkoekj <50798931+Lekrkoekj@users.noreply.github.com>
2019-06-01 23:06:25 +0200

    fg

---
+++ dark-mode.js
@@ -0,0 +1,9 @@
+function darkmode() {
+    localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark'); localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+}
+
+
+function remembertheme() {
+document.addEventListener('DOMContentLoaded', (event) => {
+  ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+})}
