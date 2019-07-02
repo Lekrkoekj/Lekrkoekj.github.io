ecc33818ec438f4303bc09e1412d46dec8a92247
Lekrkoekj <50798931+Lekrkoekj@users.noreply.github.com>
2019-07-02 19:56:33 +0200

    0123

--- effects.js
+++ effects.js
@@ -15,7 +15,6 @@ function removeBlockedOverlay() {
     
 }  
 
-
 function darkModeSwitch() {
 localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark'); localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
 }
