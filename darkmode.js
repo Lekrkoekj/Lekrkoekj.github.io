ebc1562cf0b17cf00c069bb7b023bd84399f78b5
Lekrkoekj <50798931+Lekrkoekj@users.noreply.github.com>
2019-08-04 19:42:54 +0200

    Added separate files for scripts + easier html

---
+++ darkmode.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+  })
